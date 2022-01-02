b6988b240a730aade3f246787b482d591704a31e
mc11cof <marcalistaircof@gmail.com>
2022-01-02 13:36:33 -0500

    Pushing source code

---
+++ graphql_api/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+
+# Create your models here.
+class User(models.Model):
+    name = models.CharField(max_length=250)
+    followers = models.ManyToManyField('graphql_api.User')
+
+
+class Post(models.Model):
+    content = models.TextField()
+    created_by = models.ForeignKey(User, on_delete=models.DO_NOTHING)
